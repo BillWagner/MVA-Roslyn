8d9c658ab5235862021a7242b6d358b2328c3f02
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-12 12:07:50 -0400

    Add Sample project for first module
    This is the example code that shows some poor practices. In this module,
    we'll examine the code and build a diagnostic and code fix to correct
    these mistakes.

---
+++ StudentLibrary/StudentLibrary/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    public class Student
+    {
+        public string firstName;
+        public string lastName;
+        public int pointsEarned;
+
+        public void TakeExam(int pointsForExam)
+        {
+            this.pointsEarned += pointsForExam;
+        }
+
+        public void ExtraCredit(int extraPoints)
+        {
+            this.pointsEarned += extraPoints;
+        }
+
+        public int PointsEarned { get { return pointsEarned; } }
+    }
+}
