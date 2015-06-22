47f0b5a41b550c0f1223ea5cc662f6c154776db0
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-22 12:27:04 -0400

    Add template based analyzer for it then braces

---
+++ CodingStandards/CodingStandards/CodingStandards/IfBraceAnalyzercs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodingStandards
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class IfBraceAnalyzercs : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "IfBraceAnalyzercs";
+        internal static readonly LocalizableString Title = "IfBraceAnalyzercs Title";
+        internal static readonly LocalizableString MessageFormat = "IfBraceAnalyzercs '{0}'";
+        internal const string Category = "IfBraceAnalyzercs Category";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+        }
+    }
+}
