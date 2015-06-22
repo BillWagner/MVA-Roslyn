aedfe8a819991f2be93cfe0b4fde198f80bd342e
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-22 11:47:22 -0700

    add first code fix for pure methods

---
+++ CodingStandards/CodingStandards/CodingStandards/PureMethodsFixer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Composition;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace CodingStandards
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(PureMethodsFixer)), Shared]
+    public class PureMethodsFixer : CodeFixProvider
+    {
+        // TODO: Replace with actual diagnostic id that should trigger this fix.
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(PureMethodsAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
