c7f1f16d19034619d79cd06ee5a68f66e3ac6b3a
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-22 09:48:50 -0700

    Add the analyzer for the pure methods analyzer
    Ready to start implementing.

---
+++ CodingStandards/CodingStandards/CodingStandards/PureMethodsAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodingStandards
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PureMethodsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "PureMethodsAnalyzer";
+        internal static readonly LocalizableString Title = "Ignored Method return code";
+        internal static readonly LocalizableString MessageFormat = "The return value from '{0}' is being ignored";
+        internal const string Category = "CodingStandards";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+        }
+    }
+}
