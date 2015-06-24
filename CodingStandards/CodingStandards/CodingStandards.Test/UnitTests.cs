a8f34540f05053db0575e9c1f3e4a4b206415c13
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-23 22:03:05 -0700

    Create what should be a failing test
    Interestingly, the lower level libraries catch the exception, so it's
    hard to find. But, we can debug and trap

--- CodingStandards/CodingStandards/CodingStandards.Test/UnitTests.cs
+++ CodingStandards/CodingStandards/CodingStandards.Test/UnitTests.cs
@@ -23,7 +23,7 @@ namespace CodingStandards.Test
 
         //Diagnostic and CodeFix both triggered and checked for
         [TestMethod]
-        public void TestMethod2()
+        public void LargeIntegrationTest()
         {
             var test = @"
 using System;
@@ -82,7 +82,7 @@ namespace StudentLibrary
 
             VerifyCSharpDiagnostic(test, expected);
 
-            var fixtest = @"
+            var fixtest1 = @"
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -106,9 +106,87 @@ namespace StudentLibrary
     }
 }
 ";
-            VerifyCSharpFix(test, fixtest, 1);
+            VerifyCSharpFix(test, fixtest1, 1);
         }
 
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public void RecoverFromInvalidSourceTree()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    public class Student
+    {
+        public string firstName;
+        public string lastName;
+        public string GetFormattedName()
+        {
+            var name = firstName;
+            var name.PadRight(20);
+            string.Concat(name, lastName);
+            name.PadRight(40);
+            return name;
+        }
+    }
+}
+";
+            var expected = new DiagnosticResult[]
+            {
+            new DiagnosticResult{
+                Id = PureMethodsAnalyzer.DiagnosticId,
+                Message = String.Format("The return value from '{0}' is being ignored", "Concat"),
+                Severity = DiagnosticSeverity.Info,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 18, 13)
+                        }
+            },
+            new DiagnosticResult{
+                Id = PureMethodsAnalyzer.DiagnosticId,
+                Message = String.Format("The return value from '{0}' is being ignored", "PadRight"),
+                Severity = DiagnosticSeverity.Info,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 19, 13)
+                        }
+            }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+            var fixtest1 = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    public class Student
+    {
+        public string firstName;
+        public string lastName;
+        public string GetFormattedName()
+        {
+            var name = firstName;
+            var name.PadRight(20);
+            var returnValue = string.Concat(name, lastName);
+            name.PadRight(40);
+            return name;
+        }
+    }
+}
+";
+            VerifyCSharpFix(test, fixtest1, 0);
+        }
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
             return new PureMethodsFixer();
